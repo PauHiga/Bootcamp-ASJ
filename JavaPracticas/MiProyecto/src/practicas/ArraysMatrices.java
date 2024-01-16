da34aa59a46f53bf561508a4a179f97bcc43505b
PauHiga <p.gabriela.higa@gmail.com>
2024-01-16 11:10:24 -0300

    Commit con eclipse

---
+++ JavaPracticas/MiProyecto/src/practicas/ArraysMatrices.java
@@ -0,0 +1,70 @@
+package practicas;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArraysMatrices {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+//		Escribe un programa que lea 15 números por teclado y que los almacene en un array. 
+//		Rota los elementos de ese array, es decir, el elemento de la posición 0 debe pasar a la posición 1, el de la 1 a la 2, etc. 
+//		El número que se encuentra en la última posición debe pasar a la posición 0. 
+//		Finalmente, muestra el contenido del array.
+		
+//		int[] array15 = new int[15];
+//		int[] nuevoArray15 = new int[15];
+//		
+//		for(int i = 0; i < 15; i++) {
+//			System.out.println("Introduzca el número " + (i + 1));
+//			array15[i] = inputANumber(scanner);
+//		}
+//		
+//		for(int i = 0; i < 15; i++) {
+//			nuevoArray15[14-i] = array15[i];
+//		}
+//
+//		for(int i = 0; i < 15; i++) {
+//			System.out.print(nuevoArray15[i] + " ");	
+//		}
+		
+		
+//		-----------------------------------------------------------------------------------------
+//		-----------------------------------------------------------------------------------------
+		
+//		Escribe un programa que genere 20 números enteros aleatorios entre 0 y 100 y que los almacene en un array. 
+//		El programa debe ser capaz de pasar todos los números pares a las primeras posiciones del array (del 0 en adelante) y todos los números impares a las celdas restantes. 
+//		Utiliza arrays auxiliares si es necesario.		
+		
+		int[] randomNumbers = new int[20];
+		
+		for (int i = 0; i < 20; i++) {
+			randomNumbers[i] = (int) (Math.random() * 100) + 1;
+		}
+	
+		System.out.println("Array inicial: ");
+		for (int i = 0; i < 20; i++) {
+			System.out.print(randomNumbers[i] + " ");
+		}
+		
+		
+		
+	}
+
+	
+	public static int inputANumber(Scanner scanner) {
+		String input;
+		int inputNumber;
+		while(true) {
+			try {
+				input = scanner.nextLine();				
+				inputNumber = Integer.valueOf(input);
+				return inputNumber;
+			} catch (Exception e) {
+				System.out.println("El valor introducido no es un número. Por favor introduzca un número");
+			}
+		}
+	}
+}
--- JavaPracticas/MiProyecto/src/practicas/funciones.java
+++ JavaPracticas/MiProyecto/src/practicas/funciones.java
@@ -1,10 +1,143 @@
 package practicas;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class funciones {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double saldo = 100.00;	
+		
+		List<String> movimientos = new ArrayList<String>();
+		
+		Scanner scanner =  new Scanner(System.in);
+		boolean exit = true;
+		
+		while(exit) {
+			System.out.println("-------------------------------------------");
+			System.out.println("Seleccione una opción");
+			System.out.println("-------------------------------------------");
+			System.out.println("1 - Extraer");
+			System.out.println("2 - Depositar");
+			System.out.println("3 - Consultar saldo");
+			System.out.println("4 - Ver últimos movimientos");
+			System.out.println("5 - Salir");
 
+			System.out.println("-------------------------------------------");
+
+			int selectedOption = getUserInput(scanner);
+			double nuevoSaldo;
+			
+			System.out.println("");
+
+			switch(selectedOption) {
+				case 1:
+					nuevoSaldo = extraer(scanner, saldo, movimientos);
+					saldo = nuevoSaldo;
+					break;
+				case 2:
+					nuevoSaldo = depositar(scanner, saldo, movimientos);
+					saldo = nuevoSaldo;
+					break;
+				case 3:
+					consultar(saldo);
+					break;
+				case 4:
+					verMovimientos(movimientos);
+					break;
+				case 5:
+					exit = false;
+					break;
+			}
+			
+			System.out.println("");
+			
+		}
+		
+		System.out.println("-------------------------------------------");
+		System.out.println("Gracias por utilizar el servicio");
+		System.out.println("Que tenga un buen dia");
+		System.out.println("-------------------------------------------");
+		
+	}
+	
+	public static int getUserInput(Scanner scanner) {
+		String userInput;
+		while(true) {
+			System.out.println("Introduzca la opción elegida");
+			userInput = scanner.nextLine();
+			try {
+				int userInputNumber = Integer.valueOf(userInput);
+				if(userInputNumber >=1 && userInputNumber<=5) {
+					return userInputNumber;
+				}
+				System.out.println("Por favor introduzca un número del 1 al 5");				
+			}
+			catch (Exception e) {
+				System.out.println("Por favor introduzca un número del 1 al 5");								
+			}
+		}
+	}
+	
+	public static double getUserInputNumber(Scanner scanner) {
+		String userInput;
+		while(true) {
+			System.out.println("Por favor, introduzca el monto");
+			userInput = scanner.nextLine();
+			try {
+				double userInputNumber = Double.valueOf(userInput);
+				if(userInputNumber >=0) {
+					return userInputNumber;
+				}
+				System.out.println("El monto debe ser un número positivo");				
+			}
+			catch (Exception e) {
+				System.out.println("El valor introducido es inválido");								
+			}
+		}
+	}
+	
+	public static double extraer(Scanner scanner, double importeActual, List<String> movimientos) {
+		System.out.println("Usted seleccionó: Extraer");
+		double importeAExtraer;
+		while(true) {
+			System.out.println("El monto disponible es: " + importeActual);
+			importeAExtraer = getUserInputNumber(scanner);			
+			if(importeAExtraer > importeActual) {
+				System.out.println("El importe seleccionado supera el monto actual");				
+			} else {
+				break;
+			}
+		}
+		double total = importeActual - importeAExtraer;
+		System.out.println("Se extrajo: " + importeAExtraer);				
+		System.out.println("Total en cuenta: " + total);		
+		movimientos.add("Extracción - $" + importeAExtraer + " ----- Monto total: $" + total );
+		return total;
+	}
+	
+	public static double depositar(Scanner scanner, double importeActual, List<String> movimientos) {
+		System.out.println("Usted seleccionó: Depositar");
+		System.out.println("El monto actual es: " + importeActual);
+		double importeADepositar = getUserInputNumber(scanner);			
+		double total = importeActual - importeADepositar;
+		System.out.println("Se depositó: " + importeADepositar);				
+		System.out.println("Total en cuenta: " + total);				
+		movimientos.add("Depósito - $" + importeADepositar + " ----- Monto total: $" + total );
+		return total;
+	}
+	
+	public static void consultar(double importeActual) {
+		System.out.println("Usted seleccionó: Consultar Saldo");
+		System.out.println("El monto actual es: " + importeActual);
+	}
+	
+	public static void verMovimientos(List <String> movimientos) {
+		for (String movimiento : movimientos) {
+			System.out.println(movimiento);
+		}
 	}
 
 }
